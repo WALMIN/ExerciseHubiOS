c3949fa9b6bc2b9a3e5247f98898830705118f82
Victor Bergene <v@walmin.com>
2021-01-29 13:53:06 +0100

    Initial Commit

---
+++ ExerciseHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExerciseHub
+//
+//  Created by Victor Bergene on 2021-01-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExerciseHub/ExerciseHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseHubApp.swift
+//  ExerciseHub
+//
+//  Created by Victor Bergene on 2021-01-29.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
